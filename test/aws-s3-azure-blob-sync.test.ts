78430755eadf1341ad7e6dc871ab2f04c67fc582
Saurabh Gupta <saurabh.gupta@bettermarks.com>
2024-09-15 11:29:01 +0200

    feat: add snapshot to test to track changes

--- test/aws-s3-azure-blob-sync.test.ts
+++ test/aws-s3-azure-blob-sync.test.ts
@@ -1,17 +1,22 @@
-// import * as cdk from 'aws-cdk-lib';
-// import { Template } from 'aws-cdk-lib/assertions';
-// import * as AwsS3AzureBlobSync from '../lib/aws-s3-azure-blob-sync-stack';
-
+import * as cdk from 'aws-cdk-lib';
+import { Template } from 'aws-cdk-lib/assertions';
+import {AwsS3AzureBlobSyncStack} from "../lib/aws-s3-azure-blob-sync-stack";
+import "jest-cdk-snapshot";
 // example test. To run these tests, uncomment this file along with the
 // example resource in lib/aws-s3-azure-blob-sync-stack.ts
 test('SQS Queue Created', () => {
-//   const app = new cdk.App();
-//     // WHEN
-//   const stack = new AwsS3AzureBlobSync.AwsS3AzureBlobSyncStack(app, 'MyTestStack');
-//     // THEN
-//   const template = Template.fromStack(stack);
+  const app = new cdk.App();
+    // WHEN
+  const stack = new AwsS3AzureBlobSyncStack(app, 'MyTestStack', {
+    bucket: "some-bucket",
+    azureStorageAccountName: "azure-storage-account-name",
+    azureStorageAccountKey: "azure-storage-account-key",
+    azureContainerName: "azure-container-name"
+  });
+    // THEN
+  const template = Template.fromStack(stack);
 
-//   template.hasResourceProperties('AWS::SQS::Queue', {
-//     VisibilityTimeout: 300
-//   });
+  expect(stack).toMatchCdkSnapshot({
+    ignoreAssets: true,
+  });
 });
